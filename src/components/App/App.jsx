3e405c988596624841442e8a8803a35dee641093
Dima <vidistudioproduction@gmail.com>
2023-05-26 17:52:18 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,7 @@ import Notification from 'components/Notification/Notification';
 import Statistics from 'components/Statistics/Statistics';
 
 const App = () => {
+  
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
